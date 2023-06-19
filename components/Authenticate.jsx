d0e82c604e718751159db7df2f63a569eceed6e0
Armaan Hossain <armaanio210@gmail.com>
2023-06-19 15:45:37 -0400

    First iteration of tapped webapp

---
+++ components/Authenticate.jsx
@@ -0,0 +1,59 @@
+import React, {useEffect, useState} from "react";
+import { auth } from "../config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import Link from 'next/link';
+import Image from 'next/image'
+
+const Authenticate = () => {
+
+  const [authUser, setAuthUser] = useState("");
+  useEffect(() => {
+    const listenAuth = onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setAuthUser(user);
+        } else {
+            setAuthUser(null);
+        }
+    })
+    return () => {
+        listenAuth();
+    }
+  },[])
+
+  const userSignOut = () => {
+    auth.signOut().catch(error => console.log("error"))
+  }
+
+  return (
+    <div className='sm:flex hidden'>
+        { authUser === null ?
+        <div className='flex gap-3 md:gap-5'>
+            <Link href='/login'>
+                <button className='tapped_btn'>
+                    Login
+                </button>
+            </Link>
+        </div> :
+
+        <div className='flex gap-3 md:gap-5'>
+
+            <button onClick={userSignOut} type='button' className='tapped_btn'>
+                Sign Out
+            </button>
+
+            <Link href='/profile'>
+                <Image
+                src='/assets/images/profile.jpg'
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
+                />
+            </Link>
+
+        </div>}
+    </div>
+  )
+}
+
+export default Authenticate
---
+++ components/Feed.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Feed = () => {
+  return (
+    <div><p className='tapped_txt_blue'>Feed</p></div>
+  )
+}
+
+export default Feed
